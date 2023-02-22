34cb6158ae75597062c0d86346e5d1fcb5a6cdc9
Lascabim <josepedrogomes27@gmail.com>
2023-02-22 22:39:39 +0000

    22.02.2023 Footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <nav class="flex justify-between px-10 h-24 items-center bg-zinc-800">
+        <nav class="fixed z-10 top-0 left-0 right-0 flex justify-between px-10 h-24 items-center bg-zinc-800">
 
             <div class="flex items-center gap-x-5">
                 <a href="https://www.instagram.com/ambulanceforhearts/"><img style="width:50px; height:40px;" src="https://www.ambulanceforhearts.pt/img/icons/instagram.svg" alt="Instagram"></a>
@@ -29,8 +29,14 @@
 
         </nav>
 
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="pt-24 pb-2 font-sans text-gray-900 antialiased">
             {{ $slot }}
+        </div>
+        
+        <div class="fixed z-10 bottom-0 left-0 right-0 bg-zinc-900 flex items-center justify-center">
+            <a href="https://www.ambulanceforhearts.pt/">
+                <p class="text-white p-2">David Freitas © Ambulance For Earths</p>
+            </a>
         </div>
     </body>
 </html>
